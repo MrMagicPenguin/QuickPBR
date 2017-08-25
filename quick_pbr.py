afcd5ffaa4ae8f1f27d293fe0af9436475f19b13
MrMagicPenguin <noah.price.237@gmail.com>
2017-08-24 22:40:38 -0400

    Changed convenience variables to be global, changed operator name
    - Moved Convenience Variables above function definitions so they can be
    globally acessed by the functions
    - Removed clearNodes() as a function, moved the functionality to be
    global.
    - While clearNodes was a function, after it was called it would run
    constantly. While it is a global variable, it runs once. I don't know
    why this is...
    
    - updated and edited comments to keep in line with current style

--- quick_pbr.py
+++ quick_pbr.py
@@ -1,4 +1,40 @@
 import bpy
+    
+mat_name = bpy.context.active_object.active_material.name
+mat = (bpy.data.materials.get(mat_name) or bpy.data.materials.new(mat_name))
+mat.use_nodes = True
+
+nt = mat.node_tree
+nodes = nt.nodes
+links = nt.links
+
+image_path = "C:\\Users\\Noah\\Desktop\\MultiMedia\\Blender\\Scripts\\QuickPBR Assets\\PBRTest Folder\\example_Col1.png"
+image_path2 = "C:\\Users\\Noah\\Desktop\\MultiMedia\\Blender\\Scripts\\QuickPBR Assets\\PBRTest Folder\\example_nrm.png"
+image_path3 = "C:\\Users\\Noah\\Desktop\\MultiMedia\\Blender\\Scripts\\QuickPBR Assets\\PBRTest Folder\\example_spec.png"
+
+
+#clear all nodes
+while (nodes): nodes.remove(nodes[0])
+#Map node names
+colorMap = nodes.new("ShaderNodeTexImage")
+normalMap = nodes.new("ShaderNodeTexImage")
+roughMap = nodes.new("ShaderNodeTexImage")#.color_space = 'NONE'
+
+#Utility node names
+output = nodes.new("ShaderNodeOutputMaterial")
+principled = nodes.new ("ShaderNodeBsdfPrincipled")
+mapping = nodes.new("ShaderNodeMapping")
+texCoord = nodes.new("ShaderNodeTexCoord")
+
+mainTexCoord = [ node for node in mat.node_tree.nodes if node.bl_idname=="ShaderNodeTexCoord"]
+# python list comprehension filters the list of nodes down to the ones that match the right bl_idname
+texCoordinateNode = mainTexCoord[0]
+
+#Image Texture Node Image
+colorMap.image = bpy.data.images.load(image_path)
+normalMap.image = bpy.data.images.load(image_path2)
+roughMap.image = bpy.data.images.load(image_path2)
+
 
 def makeLink(var1, var2, var3, var4):
     """
@@ -24,7 +60,28 @@ def makeSpec():
     makeLink(principled, "Metallic", roughMap, 'Color')
     makeLink(roughMap, 'Vector', mapping, 'Vector')
     
+def createNodes():
+    defaultLinks()
+    makePrincipled()
+    makeNormal()
+    makeSpec()
+    
 def sortNodeNames():
+    #Sort node tree for Texture Images
+    image_texture_nodes = [n for n in nt.nodes if n.type == "TEX_IMAGE"]
+    # Extract the name of the node and place in list
+    image_texture_names = [n.name for n in image_texture_nodes]
+    #Filter out image textures
+    node_types = [n for n in nt.nodes if n.type != "TEX_IMAGE"]
+    # Extract string name and place in new node
+    # Reversed so it orders them correctly
+    node_names = [n.name for n in reversed(node_types)]
+    #insert image_texture_names at the correct index 
+    for i in image_texture_names:
+        node_names.insert(2,i)
+    return node_names
+        
+def setLocation():
     #Sort node tree for Texture Images
     image_texture_nodes = [n for n in nt.nodes if n.type == "TEX_IMAGE"]
     # Extract the name of the node and place in list
@@ -36,9 +93,8 @@ def sortNodeNames():
     node_names = [n.name for n in reversed(node_types)]
     #insert image_texture_names at the correct index 
     for i in image_texture_names:
-        node_names.insert(2,i) 
+        node_names.insert(2,i)
         
-def setLocation():
     startlocx = 0
     startlocy = 0
     
@@ -56,85 +112,42 @@ def setLocation():
             locx += nodes[lastnode].width + 50
         nodes[node].location.x = locx 
         nodes[node].location.y = locy
-def clearNodes():
-    mat_name = bpy.context.active_object.active_material.name
-    mat = (bpy.data.materials.get(mat_name) or bpy.data.materials.new(mat_name))
-    mat.use_nodes = True
-
-    nt = mat.node_tree
-    nodes = nt.nodes
-    links = nt.links
-    while (nodes): nodes.remove(nodes[0])
-
-def main(context):
-    mat_name = bpy.context.active_object.active_material.name
-    mat = (bpy.data.materials.get(mat_name) or bpy.data.materials.new(mat_name))
-    mat.use_nodes = True
-
-    nt = mat.node_tree
-    nodes = nt.nodes
-    links = nt.links
-
-    image_path = "C:\\Users\\lori-laptop\\Desktop\\PBRTest Folder\\example_Col1.png"
-    image_path2 = "C:\\Users\\lori-laptop\\Desktop\\PBRTest Folder\\example_nrm.png"
-    image_path3 = "C:\\Users\\lori-laptop\\Desktop\\PBRTest Folder\\example_spec.png"
-
-
-    #clear all nodes
-    clearNodes()
-
-    #Map node names
-    colorMap = nodes.new("ShaderNodeTexImage")
-    normalMap = nodes.new("ShaderNodeTexImage")
-    roughMap = nodes.new("ShaderNodeTexImage")#.color_space = 'NONE'
-
-    #Utility node names
-    output = nodes.new("ShaderNodeOutputMaterial")
-    principled = nodes.new ("ShaderNodeBsdfPrincipled")
-    mapping = nodes.new("ShaderNodeMapping")
-    texCoord = nodes.new("ShaderNodeTexCoord")
-
-    mainTexCoord = [ node for node in mat.node_tree.nodes if node.bl_idname=="ShaderNodeTexCoord"]
-    # python list comprehension filters the list of nodes down to the ones that match the right bl_idname
-    texCoordinateNode = mainTexCoord[0]
-
-    #Image Texture Node Image
-    colorMap.image = bpy.data.images.load(image_path)
-    normalMap.image = bpy.data.images.load(image_path2)
-    roughMap.image = bpy.data.images.load(image_path2)
+        
+#def clearNodes():
     
-    makePrincipled()
-    makeNormal()
-    makeSpec()
+class main():  
+    # Create nodes
+    createNodes()
+    print("Creating Nodes!")
+        
+    # Sort node names
     sortNodeNames()
+    print("Sorting Node Names!!")
+    
+    # Set node location
     setLocation()
+    print("Setting Nodes!")
     
-    
-
-class SimpleOperator(bpy.types.Operator):
+class QuickLayout(bpy.types.Operator):
     """Tooltip"""
-    bl_idname = "object.simple_operator"
-    bl_label = "Simple Object Operator"
-
-    @classmethod
-    def poll(cls, context):
-        return context.active_object is not None
+    bl_idname = "rug.quick_layout"
+    bl_label = "Quick PBR Layout"
 
     def execute(self, context):
-        main(context)
+        main()       
         return {'FINISHED'}
 
 
 def register():
-    bpy.utils.register_class(SimpleOperator)
+    bpy.utils.register_class(QuickLayout)
 
 
 def unregister():
-    bpy.utils.unregister_class(SimpleOperator)
+    bpy.utils.unregister_class(QuickLayout)
 
 
 if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.object.simple_operator()
+    #bpy.ops.object.simple_operator()
